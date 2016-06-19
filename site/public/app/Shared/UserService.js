b4cbc80c9e455eff0717f35eb3ff502fc71e11af
Derek Hunter <derek.hunter@aggiemail.usu.edu>
2016-06-19 15:57:59 -0600

    User Service work

---
+++ site/public/app/Shared/UserService.js
@@ -0,0 +1,4 @@
+angular.module('Podcastio').factory('UserService', function(){
+    var UserService = this;
+    return UserService;
+});
--- site/public/app/login_page/LoginPageController.js
+++ site/public/app/login_page/LoginPageController.js
@@ -1,10 +1,9 @@
-angular.module('Podcastio').controller('LoginPageCtrl', function($scope, $firebaseAuth) {
+angular.module('Podcastio').controller('LoginPageCtrl', function($scope, $firebaseAuth, UserService) {
 
     $scope.login = function(){
         var auth = $firebaseAuth();
         auth.$signInWithPopup("google").then(function(firebaseUser){
-                console.log("Signed in as: ", firebaseUser);
-                alert("Hello " + firebaseUser.user.displayName);
+                UserService = firebaseUser;
         }).catch(function(error){
                 console.log("Authentication failed:", error);
         })
