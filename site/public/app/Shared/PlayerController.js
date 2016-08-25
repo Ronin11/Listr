f04fc188a1454e3d7a546107d82f41dacaf6d041
Derek Hunter <derek.hunter@aggiemail.usu.edu>
2016-08-24 18:38:33 -0600

    Adds support for play pause?

--- site/public/app/Shared/PlayerController.js
+++ site/public/app/Shared/PlayerController.js
@@ -1,44 +1,17 @@
-angular.module('Podcastio').controller('PlayerCtrl', function($http, $scope, $firebaseAuth, $mdSidenav) {
-
+angular.module('Podcastio').controller('PlayerCtrl', function($http, $scope, $firebaseAuth, $document) {
     PlayerCtrl = this;
-    $scope.ctrl = PlayerCtrl;
     PlayerCtrl.playing = false;
-
-    var songBuffer = null;
-    // Fix up prefixing
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    var context = new AudioContext();
-
-    // function loadSong(url) {
-    //     var request = new XMLHttpRequest();
-    //     request.open('GET', url, true);
-    //     request.responseType = 'arraybuffer';
-
-    //     // Decode asynchronously
-    //     request.onload = function() {
-    //         context.decodeAudioData(request.response, function(buffer) {
-    //         songBuffer = buffer;
-    //         }, onError);
-    //     }
-    //     request.send();
-    // }
-
-    PlayerCtrl.audioUrl = 'http://traffic.libsyn.com/sawbones/Sawbones146Tea.mp3'
+    var audioElement = $document[0].createElement('audio');
     
-    audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    
-    PlayerCtrl.playpause = function(){
-        var source = audioCtx.createBufferSource(); // creates a sound source
-        source.buffer = songBuffer;                    // tell the source which sound to play
-        source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-        source.start(0);                           // play the source now
-                                             // note: on older systems, may have to use deprecated noteOn(time);
-        if(!PlayerCtrl.playing){
-            gainNode.connect(audioCtx.destination);
-        }else{
-            gainNode.disconnect(audioCtx.destination);
-        }
-        PlayerCtrl.playing = !PlayerCtrl.playing;
+    PlayerCtrl.togglePlay = function(){
+      console.log("Butts");
+      audioElement.src = "path"
+      if(!PlayerCtrl.playing){
+          audioElement.play();
+      }else{
+        audioElement.pause();
+      }
+      PlayerCtrl.playing = !PlayerCtrl.playing;
     }
 
 });
