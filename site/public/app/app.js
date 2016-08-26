92f297e0e52ee9c4d14feb53a1ccdfa40b0462d7
Derek Hunter <derek.hunter@aggiemail.usu.edu>
2016-08-26 13:08:00 -0600

    Some cool design Cleanup

--- site/public/app/app.js
+++ site/public/app/app.js
@@ -2,34 +2,39 @@ app = angular.module('Podcastio', [
   'ngMaterial',
   'lfNgMdFileInput',
   'ngRoute',
-  'firebase'
+  'firebase',
+  'xml'
 ]);
 
-app.config(['$routeProvider', '$mdThemingProvider', '$httpProvider', function($routeProvider, $mdThemingProvider, $httpProvider){
-  $httpProvider.defaults.useXDomain = true;
-  delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  
-  
-  $mdThemingProvider.theme('default')
-    .primaryPalette('deep-purple')
-    .accentPalette('orange');
 
-  $routeProvider.
-  when('/home', {
-    templateUrl: 'app/home_page/HomePage.html',
-    controller: 'HomePageCtrl'
-  }).
-  when('/login', {
-    templateUrl: 'app/login_page/LoginPage.html',
-    controller: 'LoginPageCtrl'
-  }).
-  when('/player', {
-    templateUrl: 'app/player_page/PlayerPage.html',
-    controller: 'PlayerPageCtrl'
-  }).
-  when('/upload', {
-    templateUrl: 'app/upload_page/UploadPage.html',
-    controller: 'UploadPageCtrl'
-  }).
-  otherwise('/home');
+
+app.config(['$routeProvider', '$mdThemingProvider', '$httpProvider',
+  function($routeProvider, $mdThemingProvider, $httpProvider){
+    $httpProvider.defaults.useXDomain = true;
+    $httpProvider.interceptors.push('xmlHttpInterceptor');
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    
+    
+    $mdThemingProvider.theme('default')
+      .primaryPalette('deep-purple')
+      .accentPalette('orange');
+
+    $routeProvider.
+    when('/home', {
+      templateUrl: 'app/home_page/HomePage.html',
+      controller: 'HomePageCtrl'
+    }).
+    when('/login', {
+      templateUrl: 'app/login_page/LoginPage.html',
+      controller: 'LoginPageCtrl'
+    }).
+    when('/player', {
+      templateUrl: 'app/player_page/PlayerPage.html',
+      controller: 'PlayerPageCtrl'
+    }).
+    when('/upload', {
+      templateUrl: 'app/upload_page/UploadPage.html',
+      controller: 'UploadPageCtrl'
+    }).
+    otherwise('/home');
 }]);
